1e7e666c0af8b6886a90d56d4adcc2e6c7e5efaf
Euan-Mushens <euanmush@shaw.ca>
2023-12-21 10:08:26 -0700

    Pond scene added

---
+++ Assets/Scripts/gri.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gri : MonoBehaviour
+{
+    public void NextGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+}
